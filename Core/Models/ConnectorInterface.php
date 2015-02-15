a7630b1dfa856024ba6b4cd655396b4069f2aa6b
sepehr@live.co.uk <sepehr@live.co.uk>
2015-02-15 21:08:45 +0000

    added functions to connector interface

--- Core/Models/ConnectorInterface.php
+++ Core/Models/ConnectorInterface.php
@@ -3,6 +3,8 @@ namespace BrightSkies\IrcBotPhp\Core\Models;
 
 interface ConnectorInterface
 {
+    function __construct($address, $port);
+    public function connect();
     public function send($message);
     public function receive();
 }
